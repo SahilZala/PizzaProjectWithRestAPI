d4133dffe9871cad359f6c8085bd6074da73379a
sahilz <zala4011@gmail.com>
2022-09-26 11:07:55 +0530

    non

--- src/main/java/com/pack/repository/UserModelRepository.java
+++ src/main/java/com/pack/repository/UserModelRepository.java
@@ -2,8 +2,8 @@ package com.pack.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.pack.model.UserModel;
 
@@ -11,7 +11,7 @@ public interface UserModelRepository extends JpaRepository<UserModel, Integer>{
 	
 	public Optional<UserModel> findByUserName(String userName);
 	
-	@Query("select u from user u where u.userName = ?1 and u.password = ?2")
+	@Query("select u from UserModel u where u.userName = ?1 and u.password = ?2")
 	public Optional<UserModel> findByUserNameAndPassword(String userName,String password);
 	
 }
